fd632dccc03fdeb4590496d132f8fe842916d968
Bogdan Petraszczuk <bp372955@students.mimuw.edu.pl>
2023-08-19 23:05:52 +0200

    Added main.cpp

---
+++ multiplicity/main.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+static vector<tuple<int,char,uint64_t>> remove_copies(vector<tuple<int,char,int>>& original_intervals) {
+    uint64_t sum;
+    int i = 0;
+    int get_number;
+    char get_color;
+    int length = original_intervals.size();
+    vector<tuple<int,char,uint64_t>> without_copies;
+
+    while (i < length) {
+        get_number = get<0>(original_intervals[i]);
+        get_color = get<1>(original_intervals[i]);
+        sum = 0;
+
+        if (i + 1 < length && std::get<0>(original_intervals[i + 1]) == get_number &&
+              get<1>(original_intervals[i + 1]) == get_color) {
+                    int j = i;
+
+                    while (j < length &&
+                            get<0>(original_intervals[j]) == get_number &&
+                            get<1>(original_intervals[j]) == get_color) {
+                                sum += get<2>(original_intervals[j]);
+                                j++;
+                    }
+
+              without_copies.emplace_back(get_number, get_color, sum);
+              i = j;
+        }
+        else {
+            without_copies.emplace_back(get_number, get_color, get<2>(original_intervals[i]));
+            i++;
+        }
+    }
+
+    return without_copies;
+}
+
+static void generate_partition(vector<tuple<int,char,uint64_t>>& without_copies,
+                               map<uint64_t, uint64_t>& final_map) {
+    uint64_t sum;
+    int index = 1;
+    int length = without_copies.size();
+
+    int start = get<0>(without_copies[0]);
+    sum = get<2>(without_copies[0]);
+
+    while (index < length) {
+        char znak = get<1>(without_copies[index]);
+        int koniec = get<0>(without_copies[index]);
+        uint64_t degree = get<2>(without_copies[index]);
+
+        if (znak == '+') {
+            final_map[sum] += (koniec - start);
+            start = koniec;
+            sum += degree;
+            index++;
+        }
+        else {
+            if (start == koniec) {
+                final_map[sum] += 1;
+                sum -= degree;
+
+                if (sum != 0) {
+                    start++;
+                    index++;
+                }
+                else {
+                    index++;
+
+                    if (index < length) {
+                        start = get<0>(without_copies[index]);
+                        sum = get<2>(without_copies[index]);
+                        index++;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+            else {
+                final_map[sum] += koniec - start + 1;
+                sum -= degree;
+
+                if (sum != 0) {
+                    start = koniec + 1;
+                    index++;
+                }
+                else {
+                    index++;
+
+                    if (index < length) {
+                        start = get<0>(without_copies[index]);
+                        sum = get<2>(without_copies[index]);
+                        index++;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    int a,b,c;
+    int local_index1 = 0;
+    map<uint64_t, uint64_t> final_map;
+    vector<tuple<int, char, int>> original_intervals(2 * n);
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> a >> b >> c;
+
+        original_intervals[local_index1] = {a, '+', c};
+        original_intervals[local_index1 + 1] = {b, '-', c};
+
+        local_index1 = local_index1 + 2;
+    }
+
+    sort(original_intervals.begin(), original_intervals.end());
+    auto without_copies = remove_copies(original_intervals);
+    generate_partition(without_copies, final_map);
+
+    if (final_map.size() == 1) {
+        cout << 0 << "\n";
+
+        return 0;
+    }
+
+    uint64_t unordered_pairs_number;
+    uint64_t sum_of_elements;
+    int counter = 0;
+
+    for (auto i : final_map) {
+        if (counter == 0) {
+            unordered_pairs_number = i.second;
+            sum_of_elements = i.second;
+            counter++;
+        }
+        else if (counter == 1) {
+            unordered_pairs_number *= i.second;
+            sum_of_elements += i.second;
+            counter++;
+        }
+        else {
+            unordered_pairs_number += sum_of_elements * i.second;
+            sum_of_elements += i.second;
+
+        }
+    }
+
+    std::cout << unordered_pairs_number << "\n";
+
+    return 0;
+}
